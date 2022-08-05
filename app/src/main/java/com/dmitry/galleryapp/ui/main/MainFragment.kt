9cae6109e66725d117bdfed4cba5e4d9c654af07
dmitry <piervov00@gmail.com>
2022-08-05 21:21:44 +0300

    - add permission request

--- app/src/main/java/com/dmitry/galleryapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/dmitry/galleryapp/ui/main/MainFragment.kt
@@ -1,9 +1,12 @@
 package com.dmitry.galleryapp.ui.main
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.dmitry.galleryapp.R
 import com.dmitry.galleryapp.databinding.FragmentMainBinding
@@ -24,9 +27,35 @@ class MainFragment: Fragment(R.layout.fragment_main) {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        //Получение пермишена на чтение из файловой системы
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                loadData()
+            } else {
+                Toast.makeText(context, R.string.permission_denied, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+
+    }
+
     //Зануляем байндинг во избежание утечек памяти
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 }
+
+
+private fun loadData() {
+
+}
+
