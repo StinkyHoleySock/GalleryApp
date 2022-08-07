1ce1ae9148ccfaea9f5e5e2daefd9fe9c897e33b
dmitry <piervov00@gmail.com>
2022-08-07 12:20:20 +0300

    - ImageAdapter.kt was added

--- app/src/main/java/com/dmitry/galleryapp/Gallery.kt
+++ app/src/main/java/com/dmitry/galleryapp/Gallery.kt
@@ -5,7 +5,9 @@ import android.content.Context
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
+
 import java.io.File
+
 
 class Gallery(val context: Context) {
     private val contentResolver by lazy {
@@ -19,6 +21,14 @@ class Gallery(val context: Context) {
         var uri: Uri? = null,
         var file: File? = null,
     )
+
+    class Image(
+        val id: String,
+        val name: String,
+        val uri: Uri? = null,
+        var count: Long = 0,
+    )
+
 
     fun findAlbums(): List<Album> {
 
@@ -80,5 +90,76 @@ class Gallery(val context: Context) {
         }
 
         return findAlbums.values.toList().sortedByDescending { it.count }
+    }
+
+    fun findImagesInAlbum(albumId: String): List<Image> {
+        val contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+        val projections = arrayOf(
+            MediaStore.Images.ImageColumns._ID,
+            MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME,
+            MediaStore.Images.ImageColumns.DATE_TAKEN,
+            MediaStore.Images.ImageColumns.DISPLAY_NAME,
+            MediaStore.Images.ImageColumns.ORIENTATION,
+            MediaStore.Images.ImageColumns.WIDTH,
+            MediaStore.Images.ImageColumns.HEIGHT,
+            MediaStore.Images.ImageColumns.SIZE,
+            @Suppress("DEPRECATION")
+            MediaStore.Images.ImageColumns.LATITUDE,
+            @Suppress("DEPRECATION")
+            MediaStore.Images.ImageColumns.LONGITUDE
+        )
+        val selection = "${MediaStore.Images.ImageColumns.BUCKET_ID} == ?"
+        val selectionArgs = arrayOf(albumId)
+
+        val findImages = HashMap<String, Image>()
+
+        contentResolver.query(contentUri, projections, selection, selectionArgs,
+            "${MediaStore.Images.ImageColumns.DATE_TAKEN} ASC")?.use { cursor ->
+
+            val totalCount = cursor.count
+
+            if (cursor.moveToFirst()) {
+
+                val idIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns._ID)
+                val dateTakenIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATE_TAKEN)
+                val displayNameIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DISPLAY_NAME)
+
+                val orientationIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.ORIENTATION)
+                val widthIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.WIDTH)
+                val heightIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.HEIGHT)
+                val sizeIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.SIZE)
+                @Suppress("DEPRECATION")
+                val latIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.LATITUDE)
+                @Suppress("DEPRECATION")
+                val lonIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.LONGITUDE)
+
+                do {
+                    val mediaId = cursor.getLong(idIndex)
+                    val filename = cursor.getString(displayNameIndex)
+                    val millis = cursor.getLong(dateTakenIndex)
+                    val orientation = cursor.getInt(orientationIndex)
+                    val width = cursor.getInt(widthIndex)
+                    val height = cursor.getInt(heightIndex)
+                    val size = cursor.getLong(sizeIndex)
+
+                    // scoped storage - access file via uri instead of filepath + filename
+                    var uri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, mediaId)
+
+                    val image = Image(
+                        id = mediaId.toString(),
+                        name = filename,
+                        uri = uri,
+                        count = 1,
+                    )
+                    findImages[mediaId.toString()] = image
+
+                    image.count++
+
+                } while (cursor.moveToNext())
+            }
+        }
+
+        return findImages.values.toList().sortedByDescending { it.name }
     }
 }
---
+++ app/src/main/java/com/dmitry/galleryapp/ui/album/ImageAdapter.kt
@@ -0,0 +1,40 @@
+package com.dmitry.galleryapp.ui.album
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.dmitry.galleryapp.Gallery
+import com.dmitry.galleryapp.databinding.ImageItemBinding
+
+class ImageAdapter(
+    private val listArray: List<Gallery.Image>,
+) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ImageItemBinding.inflate(inflater, parent, false)
+        return ImageViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        val image = listArray[position]
+        with(holder.binding) {
+
+            Glide.with(ivPreview)
+                .asBitmap()
+                .load(image.uri)
+                .into(ivPreview)
+
+            tvImageName.text = image.name
+        }
+    }
+
+    override fun getItemCount() = listArray.size
+
+
+    class ImageViewHolder(
+        val binding: ImageItemBinding
+    ) :RecyclerView.ViewHolder(binding.root)
+}
